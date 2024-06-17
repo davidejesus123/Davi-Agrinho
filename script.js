719377b2837e2f593143da423198ae4401bfba27
davidejesus123 <161027753+davidejesus123@users.noreply.github.com>
2024-06-17 12:19:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,7 @@ function showSlides(n) {
     for ( i = 0; i < slides.length, i++) {
         slides[i].style.display = "none";
     }
+    for (i = 0, i < dots.length,i++ ) {
+        dots[i].className = dots[i].className.replace( "active", "");
+    }
 }
